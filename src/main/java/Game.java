010df7344d1abceae9656fa3eeb8fd4783798f73
Amrit Gill <amritgill0428@outlook.com>
2024-02-13 13:07:04 -0800

    from vs code

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -43,7 +43,7 @@ public class Game{
         else{
           System.out.print("  ");
         }
-        System.out.print();
+        System.out.println();
       }
     }
   }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,6 +32,10 @@ For more in the future and more awesomer...
 5) You can make a new class for a turn, or a piece, or a screen painter
 */
 
+// Amrit Gil
+// TTT
+//
+
 public class Main {
   public static void main(String[] args) {
     Player p1 = new Player("p1");
